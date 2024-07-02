9aa642b3190fa4cac402dd3b62a966976409e52e
Onkar Shinde <154437119+Onkar-001@users.noreply.github.com>
2024-07-02 21:57:50 +0530

    Add files via upload

---
+++ Hackerrank1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Hackerrank1
+{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int first = sc.nextInt();
+		Double second = sc.nextDouble();
+		String third = sc.nextLine();
+		System.out.println(third);
+		System.out.println("\n"+second);
+		System.out.println("\n"+first);
+	}
+}
